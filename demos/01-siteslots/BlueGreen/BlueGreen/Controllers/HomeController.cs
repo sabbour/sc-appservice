4a7a82d504ee9ee706ea1f52a16d56105ad4f686
Ahmed Sabbour <sabb0ur@gmail.com>
2017-04-02 14:38:17 +0400

    Initial

---
+++ demos/01-siteslots/BlueGreen/BlueGreen/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BlueGreen.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.WhichEnvironment = ConfigurationManager.AppSettings["WhatIsMyEnvironment"];
+            ViewBag.ApplicationName = ConfigurationManager.AppSettings["ApplicationName"];
+            ViewBag.Color = ConfigurationManager.AppSettings["Color"];
+            ViewBag.Version = ConfigurationManager.AppSettings["Version"];
+            ViewBag.DatabaseConnectionString = ConfigurationManager.ConnectionStrings["DatabaseConnectionString"].ConnectionString;
+            return View();
+        }
+    }
+}
---
+++ demos/01-siteslots/BlueGreen/BlueGreen/obj/Debug/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BlueGreen.MvcApplication" Language="C#" %>
